5abba7a9d48f50b0c7f166d9222a2bf72446c653
abdul haris <98750614+abd-haris@users.noreply.github.com>
2025-02-12 06:36:51 +0700

    Update note-archive.js menambahkan validasi ketika item berada di note-archive

--- src/components/note-archive.js
+++ src/components/note-archive.js
@@ -1,46 +1,72 @@
-class NoteArchive extends HTMLElement {
+import notesData from "../data/data.js";
+
+class ArchivedButton extends HTMLElement {
+  _shadowRoot = null;
+  _style = null;
+
   constructor() {
     super();
-    this.attachShadow({ mode: "open" });
-    this.notes = [];
-
-    this.shadowRoot.innerHTML = `
-        <style>
-          h2 {
-            text-align: center;
-          }
-          #note-list {
-            display: grid;
-            grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-            gap: 1rem;
-            width: 90vw;
-            margin-top: 1rem;
-          }
-        </style>
-        <h2>Arsip Notes</h2>
-        <div id="note-list"></div>
-      `;
-  }
-
-  setArchiveNote(note) {
-    this.notes.push(note);
-    this.render();
+    this._shadowRoot = this.attachShadow({ mode: "open" });
+    this._style = document.createElement("style");
+    this._isArchived = notesData.map((note) => note.archived);
   }
 
   connectedCallback() {
     this.render();
   }
 
-  render() {
-    const noteList = this.shadowRoot.querySelector("#note-list");
-    noteList.innerHTML = "";
+  _updateStyle() {
+    this._style.textContent = `
+    button {
+        background-color: green;
+        color: white;
+        border: none;
+        padding: 5px 10px;
+        cursor: pointer;
+        }
+    `;
+  }
 
-    this.notes.forEach((note) => {
-      const noteItem = document.createElement("note-item");
-      noteItem.setNote(note);
-      noteList.appendChild(noteItem);
+  _emptyContent() {
+    this._shadowRoot.innerHTML = "";
+  }
+
+  render() {
+    this._updateStyle();
+    this._emptyContent();
+
+    this._shadowRoot.appendChild(this._style);
+    this._shadowRoot.innerHTML += `
+    <button>${this._isArchived ? "Archive" : "Unarchive"}</button>
+    `;
+
+    this.addEventListener("click", () => {
+      const noteItem = this.parentNode.parentNode;
+
+      if (noteItem) {
+        const noteId = noteItem._note.id;
+
+        const noteIndex = notesData.findIndex((note) => note.id === noteId);
+
+        if (noteIndex !== -1) {
+          if (this._isArchived) {
+            notesData[noteIndex].archived = true;
+            const archiveNote = document.querySelector("note-archive");
+            if (archiveNote) {
+              archiveNote.setNoteArchive(notesData[noteIndex]);
+            }
+          } else {
+            notesData[noteIndex].archived = false;
+            const noteList = document.querySelector("note-list");
+            if (noteList) {
+              noteList.setNoteList(notesData[noteIndex]);
+            }
+          }
+          noteItem.remove();
+        }
+      }
     });
   }
 }
 
-customElements.define("note-archive", NoteArchive);
+customElements.define("archive-button", ArchivedButton);
